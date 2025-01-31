b7ebaa6117df262e31724a5007f62463a9aab460
KGCAS STUDENTS <casstudent@kgisledu.com>
2025-01-31 11:32:47 +0530

    the file is tracked

---
+++ RectangleArea.java
@@ -0,0 +1,18 @@
+import java.util.Scanner;
+
+public class RectangleArea {
+    public static double calculateArea(double length, double width) {
+        return length * width;
+    }
+
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        System.out.print("Enter the length of the rectangle: ");
+        double length = scanner.nextDouble();
+        System.out.print("Enter the width of the rectangle: ");
+        double width = scanner.nextDouble();
+        double area = calculateArea(length, width);
+        System.out.println("The area of the rectangle is: " + area);
+        scanner.close();
+    }
+}
